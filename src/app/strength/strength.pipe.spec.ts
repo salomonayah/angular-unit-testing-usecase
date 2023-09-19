ba7d677f25dbf62c7cc105f70ae616d3b38233ba
Salomon AYAH <salomonayah@gmail.com>
2023-09-19 03:09:23 +0100

    Pipe testing as isolated test

---
+++ src/app/strength/strength.pipe.spec.ts
@@ -0,0 +1,18 @@
+import { StrengthPipe } from "./strength.pipe"
+
+describe("Strength pipe", () => {
+
+  it("Should return value (weak) for value less than 10", () => {
+    const thePipeIntense = new StrengthPipe();
+    const result = thePipeIntense.transform(5);
+    expect(result).toBe("5 (weak)");
+  })
+
+
+  it("Should return value (strong) for value more than 10 and less than 20", () => {
+    const thePipeIntense = new StrengthPipe();
+    const result = thePipeIntense.transform(15);
+    expect(result).toBe("15 (strong)");
+  })
+  
+})
