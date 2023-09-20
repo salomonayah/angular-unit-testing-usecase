d14c3b20d7758f37c8b7101645e21ef99a0fb593
Salomon AYAH <salomonayah@gmail.com>
2023-09-20 01:36:49 +0100

    Shallow integration test: confirm Ts is link with HTML through template

--- src/app/hero/hero.component.spec.ts
+++ src/app/hero/hero.component.spec.ts
@@ -1,13 +1,14 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { HeroComponent } from "./hero.component"
 import { NO_ERRORS_SCHEMA } from "@angular/core"
+import { By } from "@angular/platform-browser"
 
 
 
 // Shallow testing
 describe("HeroComponent", () => {
 
-  let fixture: ComponentFixture<HeroComponent>;
+  let fixture: ComponentFixture<HeroComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,7 +18,7 @@ describe("HeroComponent", () => {
       schemas: [NO_ERRORS_SCHEMA]
     })
 
-    fixture = TestBed.createComponent(HeroComponent);
+    fixture = TestBed.createComponent(HeroComponent)
 
 
   })
@@ -29,7 +30,37 @@ describe("HeroComponent", () => {
       strength: 8
     }
 
-    expect(fixture.componentInstance.hero.name).toBe("Honor");
+    expect(fixture.componentInstance.hero.name).toBe("Honor")
+  })
+
+
+
+  it("should write hero.name in the template", () => {
+    fixture.componentInstance.hero = {
+      id: 2,
+      name: "Honor",
+      strength: 8
+    }
+
+    fixture.detectChanges()
+
+    expect(fixture.nativeElement.querySelector('a').textContent).toContain("Honor")
+  })
+
+
+  it("should write rank with id in the template", () => {
+    fixture.componentInstance.hero = {
+      id: 2,
+      name: "Honor",
+      strength: 8
+    }
+
+    fixture.detectChanges()
+
+    // access template using debugElement
+    let debugTemplateElement = fixture.debugElement.query(By.css('a'))
+
+    expect(debugTemplateElement.nativeElement.textContent).toContain(2);
   })
 
 })
